0258c6b1fb8b7e07186356cf8daf7654708dd94b
rnishimu22001 <rnishimu22001@yahoo.co.jp>
2020-11-29 10:54:41 +0900

    Initial Commit

---
+++ AnitPatternTests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnitPatternTests
+//
+//  Created by rnishimu on 2020/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
